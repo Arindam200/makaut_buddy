1f6c076e589884a56f5a478931182a02804d166a
Arindam Majumder <arindammajumder2020@gmail.com>
2023-09-24 00:03:05 +0530

    (fix):minor ui fix

--- src/app/components/FAQ/FAQ.jsx
+++ src/app/components/FAQ/FAQ.jsx
@@ -7,14 +7,16 @@ export default function FAQ() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
     <>
-      <div className="sm:px-32 px-10 sm:py-32 py-10 sm:flex space-y-10">
-        <div className="sm:w-1/2 flex-col items-center space-y-5">
-          <h1 className="sm:text-5xl text-3xl max-sm:text-center font-bold">
-            FAQ
-          </h1>
-          <h3 className="sm:text-xl text-sm max-sm:text-center">
-            Frequently Asked Questions
-          </h3>
+      <div className="sm:px-32 px-10 sm:py-20 py-10 sm:flex space-y-10">
+        <div className="sm:w-1/2 flex items-center justify-start">
+          <div className="space-y-5 ">
+            <h1 className="sm:text-5xl text-3xl max-sm:text-center font-bold">
+              FAQ
+            </h1>
+            <h3 className="sm:text-xl text-sm max-sm:text-center">
+              Frequently Asked Questions
+            </h3>
+          </div>
         </div>
 
         <div className="sm:w-1/2 w-full text-white">
@@ -31,6 +33,9 @@ export default function FAQ() {
               {defaultContent}
             </AccordionItem>
             <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+              {defaultContent}
+            </AccordionItem>
+            <AccordionItem key="4" aria-label="Accordion 4" title="Accordion 4">
               {defaultContent}
             </AccordionItem>
           </Accordion>
