26687d7539937f265019a9632fbf14738f14fa23
Arindam Majumder <arindammajumder2020@gmail.com>
2023-09-24 22:03:21 +0530

    (feat):added Tabs

--- src/app/components/Tabs/Tabs.jsx
+++ src/app/components/Tabs/Tabs.jsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 
 import { Tabs, Tab, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 
 export default function App() {
   const [selected, setSelected] = React.useState("photos");
@@ -8,40 +10,78 @@ export default function App() {
   return (
     <div className="flex flex-col">
       <Tabs
+        className="justify-center flex flex-col"
         size="lg"
         aria-label="Options"
         selectedKey={selected}
         onSelectionChange={setSelected}
       >
-        <Tab key="photos" title="Photos">
-          <Card>
+        <Tab className="" key="photos" title="Photos">
+          <Card className="min-h-[450px]">
             <CardBody>
-              <div className="">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+              <div className="flex gap-5">
+                <Image
+                  isZoomed
+                  //   size="md"
+                  className="h-32 w-52"
+                  alt="NextUI hero Image"
+                  src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                />
+                <div className="flex-col w-2/3 mt-5">
+                  <h3 className="text-xl font-semibold">Heading</h3>
+                  <p className="text-sm w-full">
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
               </div>
             </CardBody>
           </Card>
         </Tab>
         <Tab key="music" title="Music">
-          <Card>
-            <CardBody>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+          <Card className="min-h-[450px]">
+            <CardBody className="space-y-5">
+              <div className="flex gap-5">
+                <Image
+                  isZoomed
+                  //   size="md"
+                  className="h-32 w-52"
+                  alt="NextUI hero Image"
+                  src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                />
+                <div className="flex-col w-2/3 mt-5">
+                  <h3 className="text-xl font-semibold">Heading</h3>
+                  <p className="text-sm w-full">
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
             </CardBody>
           </Card>
         </Tab>
         <Tab key="videos" title="Videos">
-          <Card>
+          <Card className="min-h-[450px]">
             <CardBody>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              <div className="flex gap-5">
+                <Image
+                  isZoomed
+                  //   size="md"
+                  className="h-32 w-52"
+                  alt="NextUI hero Image"
+                  src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                />
+                <div className="flex-col w-2/3 mt-5">
+                  <h3 className="text-xl font-semibold">Heading</h3>
+                  <p className="text-sm w-full">
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
             </CardBody>
           </Card>
         </Tab>
