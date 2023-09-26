36c47c28637fe55673b2d56a79875b9522226701
Arindam Majumder <arindammajumder2020@gmail.com>
2023-09-27 00:48:48 +0530

    (temp):Feature box

---
+++ src/app/components/Featbox/Featbox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+
+export default function Featbox() {
+  return (
+    <>
+      <div className="box w-64 h-96 text-black space-y-5  ">
+        <div className="tri">
+          <h1 className="text-5xl text-start font-bold">1.</h1>
+          <h3 className="text-lg ">
+            All the previous year questions compiled in a bundle so that
+            practice becomes easy and ever, and chances of getting commons are
+            extremely high.
+          </h3>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/components/Features/Features.jsx
+++ src/app/components/Features/Features.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import logo_s from "../../../../public/logo_shade.svg";
+import Featbox from "../Featbox/Featbox";
 
 export default function Features() {
   return (
@@ -10,27 +11,33 @@ export default function Features() {
         <div className="space-y-10 sm:py-20 pt-16">
           <h1 className="text-5xl max-sm:text-center font-bold">Features</h1>
           <div className="flex max-sm:flex-col justify-start gap-10">
-            <div className="bg-white  w-64 h-96 text-black space-y-5 pt-10 p-5">
-              {/* <div className="bg-blue-500 h-20 w-20 rounded-full"></div> */}
+            {/* <div className="bg-white  w-64 h-96 text-black space-y-5 pt-10 p-5">
               <h1 className="text-5xl text-start font-bold">1.</h1>
               <p className="text-lg">
-              All the previous year questions compiled in a bundle so that practice becomes easy and ever, and chances of getting commons are extremely high.
+                All the previous year questions compiled in a bundle so that
+                practice becomes easy and ever, and chances of getting commons
+                are extremely high.
               </p>
             </div>
             <div className="bg-white w-64 h-96 text-black space-y-5 pt-10 p-5">
-              {/* <div className="bg-blue-500 h-20 w-20 rounded-full"></div> */}
               <h1 className="text-5xl text-start font-bold">2.</h1>
               <p className="text-lg">
-              The best YouTube tutorials that can help you one-shot each and every concepts with ultimate ease, and make the last-night prep easy af.
+                The best YouTube tutorials that can help you one-shot each and
+                every concepts with ultimate ease, and make the last-night prep
+                easy af.
               </p>
             </div>
             <div className="bg-white w-64 h-96 text-black space-y-5 pt-10 p-5">
-              {/* <div className="bg-blue-500 h-20 w-20 rounded-full"></div> */}
               <h1 className="text-5xl text-start font-bold">3.</h1>
               <p className="text-lg">
-              Classified Handwritten notes just for you guys, straight from the toppers which will give you the ultimate edge at the semester exam.
+                Classified Handwritten notes just for you guys, straight from
+                the toppers which will give you the ultimate edge at the
+                semester exam.
               </p>
-            </div>
+            </div> */}
+            <Featbox />
+            <Featbox />
+            <Featbox />
           </div>
         </div>
         <div className="hidden sm:block">
