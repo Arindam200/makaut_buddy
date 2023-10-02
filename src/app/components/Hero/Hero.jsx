5795bf739b659e095fd8dcbb8d9f8b26dcc167ce
Arindam Majumder <arindammajumder2020@gmail.com>
2023-10-02 17:20:19 +0530

    (fix):Removed Animated Image

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -3,18 +3,10 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "@nextui-org/button";
+import logo from "../../../../public/Logo_Hero.png";
 import { motion } from "framer-motion";
-import ImgLeftLogo from "../../../../public/logo_components/left.png";
-import ImgRightLogo from "../../../../public/logo_components/right.png";
-import { useScroll, useTransform } from "framer-motion";
 
 export default function Hero() {
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 2]);
-  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 2]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, -30]);
-  const revRotate = useTransform(scrollYProgress, [0, 1], [0, 30]);
-
   return (
     <>
       <div className="font-grenze sm:flex sm:h-[calc(100vh-40px)] sm:justify-between px-10 sm:px-32 gap-16 sm:min-h-[calc(100vh-40px)]">
@@ -28,8 +20,9 @@ export default function Hero() {
             <span className="sm:text-9xl font-bold text-4xl">All </span>the
             resources you want, at your fingertips.
           </motion.h1>
-          <h3 className="font-sans text-start text-2xl font-light max-sm:text-sm max-sm:text-center leading-loose text bg bg-gradient-to-r from-[rgba(225,225,225,0.80)] to-[rgba(225,225,225,0.40)] bg-clip-text text-transparent">
-            MAKAUT Buddy is the one-stop academic solution for every students of the University.
+          <h3 className="text-start text-2xl font-light max-sm:text-sm max-sm:text-center leading-loose text-[rgba(225,225,225,0.96)]">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates mollitia ipsa odio fuga quaerat.
           </h3>
           <div className="font-sans sm:space-x-5 mt-4 space-x-2 max-sm:flex">
             <Button
@@ -51,25 +44,8 @@ export default function Hero() {
             </Button>
           </div>
         </div>
-        <div className="sm:w-2/5 sm:mt-12 flex items-center justify-center">
-          <div className="grid grid-cols-2 justify-center items-end">
-            <div>
-              <motion.img
-                initial={{ translateX: "25%", opacity: 0.5 }}
-                style={{ rotate, scale, opacity }}
-                className="relative justify-end"
-                src={ImgLeftLogo.src}
-              />
-            </div>
-            <div>
-              <motion.img
-                initial={{ translateX: "-25%" }}
-                style={{ rotate: revRotate, scale, opacity }}
-                className="relative justify-start items-end"
-                src={ImgRightLogo.src}
-              />
-            </div>
-          </div>
+        <div className="sm:w-2/5 sm:mt-[70px]">
+          <img src={logo.src} />
         </div>
       </div>
     </>
