f236f55dd31ef2433bb68d31303b6a26fcee066c
Mehul Gupta <mehul.gupta@cimpress.com>
2023-10-04 01:54:37 +0530

    (feat): Add Back Button to Dashboard Page #14

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Tabs from "../components/Tabs/Tabs.jsx";
 import Dropdown from "../components/Dropdown/Dropdown.jsx";
+import Image from "next/image.js";
 import "../components/Features/index.css";
 // import { Tabs, Tab } from "@nextui-org/react";
 
@@ -11,6 +12,14 @@ export default function page() {
     <>
       <div className="trilarge max-sm:hidden bg-white rotate-180 z-40"></div>
       <div className="tridown sm:hidden bg-white rotate-180 z-40"></div>
+      <a href="/">
+        <Image 
+          src={Arrow} 
+          alt="back-arrow"
+          onClick={() => console.log("hi")}
+          className="relative md:bottom-[6.75rem] md:w-10 lg:h-10 md:left-[15px] w-[1.5rem] h-[0.75rem] bottom-[2.75rem] left-[5px] cursor-pointer"
+        />
+      </a>
       <div className=" flex items-center justify-center sm:justify-between px-10 sm:px-40 gap-16">
         <div className="">
           <div className="sm:flex">
