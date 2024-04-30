3daae2b1fa15690104f2a3a74f9586b5bfcf69af
Arindam Majumder <arindammajumder2020@gmail.com>
2024-05-01 01:30:52 +0530

    fix:updated clerk auth

--- middleware.js
+++ middleware.js
@@ -1,9 +1,24 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-export default authMiddleware({
+// // This example protects all routes including api/trpc routes
+// // Please edit this to allow other routes to be public as needed.
+// // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
+// export default authMiddleware({
+// 	publicRoutes: ["/(github|twitter|linkedin)"],
+// 	ignoredRoutes: ["/", "/(api|trpc)(.*)"],
+// 	clockSkewInMs: 100_000,
+// 	clockSkewInSeconds: 100,
+// });
+
+// export const config = {
+// 	matcher: ["/((?!.*\\..*|_next).*)", "/(dashboard)(.*)"],
+// };
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// This Middleware does not protect any routes by default.
+// See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
+export default clerkMiddleware({
 	publicRoutes: ["/(github|twitter|linkedin)"],
 	ignoredRoutes: ["/", "/(api|trpc)(.*)"],
 	clockSkewInMs: 100_000,
@@ -11,5 +26,5 @@ export default authMiddleware({
 });
 
 export const config = {
-	matcher: ["/((?!.*\\..*|_next).*)", "/(dashboard)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/(dashboard)(.*)"],
 };
