5243a2e9fa3e8780e62d44467a34e9ad66938a86
Arindam Majumder <arindammajumder2020@gmail.com>
2024-05-03 19:58:33 +0530

    fix: metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,10 +23,6 @@ export const metadata = {
   description: "MAKAUT Buddy is the one-stop academic solution for every students of the University.",
   keywords: "MAKAUT, University, Academic, Next.js, Notes, PYQ, Syllabus, Makaut Buddy, Makaut Buddy App, Makaut Buddy Website, Makaut Buddy Web App, Makaut Buddy Web Application, Makaut Buddy Web Platform, Makaut Questions,Makaut PYQ, Makaut website, Makaut Buddy Web Portal, Makaut Website",
   author: "Arindam Majumder",
-  url: "https://makaut-buddy.vercel.app",
-  twitter: "@Arindam_1729",
-  site_name: "MAKAUT Buddy  |  The Academic Solution for MAKAUT Students",
-  locale: "en_US",
 };
 
 export default function RootLayout({ children }) {
@@ -39,16 +35,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
-        <meta name="author" content={metadata.author} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:url" content={metadata.url} />
-        <meta property="og:site_name" content={metadata.site_name} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:site" content={metadata.twitter} />
-        <link rel="canonical" href={metadata.url} />
+
       </Head>
       <body className={inter.className}>
         <ClerkProvider
