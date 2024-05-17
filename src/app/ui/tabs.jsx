a4f1b99df23a99fead2248e2b886121ef5558f88
Olibhia Ghosh <olibhiag@gmail.com>
2024-05-17 14:04:15 +0530

    Fixed the tab toggle in dashboard

--- src/app/ui/tabs.jsx
+++ src/app/ui/tabs.jsx
@@ -1,13 +1,19 @@
 "use client";
+import { useState } from "react";
 
 export default function tabs() {
+  const[activetab, setActivetab] = useState("active")
+  const handleclick=(tab)=>{
+    setActivetab(tab)
+  }
+  const style ="text-black bg-[#D9D9D9] rounded-md basis-1/2 py-2 text-center"
   return (
     <div className="flex w-full p-2 border-2 border-[#FFFFFF20] rounded-lg">
-      <div className="basis-1/2 py-2 text-black text-center bg-[#D9D9D9] rounded-md">
+      <div className={`${activetab==="active"?`${style}`:"basis-1/2 py-2 text-center"}`} onClick={()=>{handleclick("active")}}>
         Search manually
       </div>
-      <div className="basis-1/2 py-2 text-[#D9D9D9] text-center">
-        Search by streams
+      <div className={`${activetab==="inactive"?`${style}`:"basis-1/2 py-2 text-center"}`} onClick={()=>{handleclick("inactive")}}>
+        Search by stream
       </div>
     </div>
   );
