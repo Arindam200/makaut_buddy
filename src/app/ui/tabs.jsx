07eb03995f776907a5b733b46a37a5c7120063ec
Olibhia Ghosh <olibhiag@gmail.com>
2024-05-17 16:51:05 +0530

    fixed the toogle feature

--- src/app/ui/tabs.jsx
+++ src/app/ui/tabs.jsx
@@ -2,17 +2,17 @@
 import { useState } from "react";
 
 export default function tabs() {
-  const[activetab, setActivetab] = useState("active")
+  const[activetab, setActivetab] = useState("tab1")
   const handleclick=(tab)=>{
     setActivetab(tab)
   }
   const style ="text-black bg-[#D9D9D9] rounded-md basis-1/2 py-2 text-center"
   return (
     <div className="flex w-full p-2 border-2 border-[#FFFFFF20] rounded-lg">
-      <div className={`${activetab==="active"?`${style}`:"basis-1/2 py-2 text-center"}`} onClick={()=>{handleclick("active")}}>
+      <div className={activetab==="tab1"?`${style}`:"basis-1/2 py-2 text-center"} onClick={()=>{handleclick("tab1")}}>
         Search manually
       </div>
-      <div className={`${activetab==="inactive"?`${style}`:"basis-1/2 py-2 text-center"}`} onClick={()=>{handleclick("inactive")}}>
+      <div className={activetab==="tab2"?`${style}`:"basis-1/2 py-2 text-center"} onClick={()=>{handleclick("tab2")}}>
         Search by stream
       </div>
     </div>
