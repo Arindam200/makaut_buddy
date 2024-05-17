0e47e53eb38807467c6e9f19b3bb737ff90bd745
Olibhia Ghosh <olibhiag@gmail.com>
2024-05-18 00:17:04 +0530

    added a small transition in toggle tabs

--- src/app/ui/tabs.jsx
+++ src/app/ui/tabs.jsx
@@ -6,7 +6,7 @@ export default function Tabs() {
   const handleclick=(tab)=>{
     setActivetab(tab)
   }
-  const style ="text-black bg-[#D9D9D9] rounded-md basis-1/2 py-2 text-center"
+  const style ="text-black bg-[#D9D9D9] rounded-md basis-1/2 py-2 text-center transition-all duration-300"
   return (
     <div className="flex w-full p-2 border-2 border-[#FFFFFF20] rounded-lg">
       <div className={activetab==="tab1"?`${style}`:"basis-1/2 py-2 text-center"} onClick={()=>{handleclick("tab1")}}>
