e56fce34729623df78cb3fdd72f98ee66ff6b15d
Arindam200 <arindammajumder2020@gmail.com>
2024-10-12 05:57:50 +0530

    ok i allow

--- src/app/api/_middleware/rateLimit.js
+++ src/app/api/_middleware/rateLimit.js
@@ -7,18 +7,27 @@ const ipRequestCounts = new Map();
 
 export function rateLimitMiddleware(handler) {
   return async function (request, ...args) {
+    
     const ip = request.ip || 'anonymous';
+
     const now = Date.now();
+
     console.log(ipRequestCounts);
+
     const requestLog = ipRequestCounts.get(ip) || [];
+
     console.log(requestLog);
+
     const recentRequests = requestLog.filter(time => now - time < rateLimitPeriod);
+
     console.log(recentRequests);
+
     if (recentRequests.length >= rateLimit) {
       return NextResponse.json({ error: 'Too many requests' }, { status: 429 });
     }
 
     recentRequests.push(now);
+
     ipRequestCounts.set(ip, recentRequests);
 
     return handler(request, ...args);
